b5d5ae2b1d12a84c18ee0a90dace59c068bc52d3
Roberto Huertas <roberto.huertas@outlook.com>
2018-04-29 18:02:48 +0200

    preparing to use comrak as our parser and ast builder

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,50 @@
 #![allow(dead_code)]
-extern crate markdown;
-
-extern crate prettify_cmark;
-use prettify_cmark::prettify;
-
 extern crate comrak;
 extern crate typed_arena;
-use comrak::nodes::{AstNode, NodeValue};
-use comrak::{format_commonmark, format_html, parse_document, ComrakOptions};
+use comrak::nodes::{AstNode, NodeValue, Ast};
+use comrak::{parse_document, ComrakOptions};
 use typed_arena::Arena;
 
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
+use std::cell::RefCell;
 
 fn main() {
-    // use_markdown();
-    use_cmark();
-    // use_prettify();
+    let file = "data/test1.md";
+    let text = read_file(file).expect(&format!("Failed to find file: {}", file));
+    // println!("{}", text);
+    let arena = Arena::new();
+    let root = parse_document(&arena, &text, &ComrakOptions::default());
+    let v: Vec<_> = root.children().collect();
+    check_first_child(&root);
+    check_second_child(&v[0].data)
 }
 
-fn use_prettify() {
-    let md = Path::new("data/test1.md");
+fn read_file(file_path: &str) -> Result<String, std::io::Error> {
     let mut tokens = String::new();
-    File::open(md).unwrap().read_to_string(&mut tokens).unwrap();
-    let output = prettify(&tokens);
-    println!("{}", output);
+    let mut f = File::open(Path::new(file_path))?;
+    f.read_to_string(&mut tokens)?;
+    Ok(tokens)
 }
+
+fn check_first_child(node: &AstNode) {
+    let fc = node.first_child().ok_or("mierda").unwrap();
+    let st = fc.data.borrow_mut().content.to_vec();
+    println!("{:?}", String::from_utf8(st).unwrap());
+    println!("{:?}", fc.data.borrow_mut());
+}
+
+fn check_second_child(node: &RefCell<Ast>) {
+    println!("{}", extract_content(&node));
+    println!("{:?}", node);
+}
+
+fn extract_content(node: &RefCell<Ast>) -> String {
+    let st = node.borrow_mut().content.to_vec();
+    String::from_utf8(st).expect("Something went wrong while transforming content to string")
+}
+
 
 fn use_cmark() {
     let arena = Arena::new();
@@ -86,27 +104,16 @@ fn use_cmark2() {
         _ => (),
     });
 
-    let mut html = vec![];
-    format_html(root, &ComrakOptions::default(), &mut html).unwrap();
-    let result = String::from_utf8(html).unwrap();
-    assert_eq!(
-        result,
-        "<p>This is your input.</p>\n\
-         <ol>\n\
-         <li>Also your input.</li>\n\
-         <li>Certainly your input.</li>\n\
-         </ol>\n"
-    );
-    println!("{}", result);
-}
-
-fn use_markdown() {
-    let md = Path::new("data/test1.md");
-    let mut tokens = String::new();
-    File::open(md).unwrap().read_to_string(&mut tokens).unwrap();
-    let md_tokens = markdown::tokenize(&tokens);
-    md_tokens.iter().for_each(|x| println!("{:?}", x));
-
-    let md_html = markdown::to_html(&tokens);
-    println!("{}", md_html);
+    // let mut html = vec![];
+    // format_html(root, &ComrakOptions::default(), &mut html).unwrap();
+    // let result = String::from_utf8(html).unwrap();
+    // assert_eq!(
+    //     result,
+    //     "<p>This is your input.</p>\n\
+    //      <ol>\n\
+    //      <li>Also your input.</li>\n\
+    //      <li>Certainly your input.</li>\n\
+    //      </ol>\n"
+    // );
+    // println!("{}", result);
 }
