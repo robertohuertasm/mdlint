01a0d561f6bae24cb5092cf27b731fd91d0666c8
Roberto Huertas <roberto.huertas@outlook.com>
2018-08-25 00:55:28 +0200

    better naming

--- mdlint/src/rules/common_checks.rs
+++ mdlint/src/rules/common_checks.rs
@@ -9,22 +9,25 @@ crate fn check_content<'a>(root: &'a AstNode<'a>, regex: &str) -> Vec<RuleResult
     nodes.iter().fold(Vec::new(), |mut acc, &n| {
         let content = extract_content(n);
         let mut last_parsed_line = 0;
-        content.split('\n').filter(|l| !l.is_empty()).for_each(|l| {
-            if rx.is_match(l) {
-                let node = n.data.borrow();
-                last_parsed_line = if last_parsed_line == node.start_line {
-                    last_parsed_line += 1;
-                    last_parsed_line
-                } else {
-                    node.start_line
-                };
-                acc.push(RuleResultDetails::new(
-                    last_parsed_line,
-                    node.start_column,
-                    l.to_string(),
-                ));
-            }
-        });
+        content
+            .split('\n')
+            .filter(|line| !line.is_empty())
+            .for_each(|line| {
+                if rx.is_match(line) {
+                    let node = n.data.borrow();
+                    last_parsed_line = if last_parsed_line == node.start_line {
+                        last_parsed_line += 1;
+                        last_parsed_line
+                    } else {
+                        node.start_line
+                    };
+                    acc.push(RuleResultDetails::new(
+                        last_parsed_line,
+                        node.start_column,
+                        line.to_string(),
+                    ));
+                }
+            });
         acc
     })
 }
