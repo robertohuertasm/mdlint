4efb1147e810a5d0febe64438768d5f5ff82beed
Roberto Huertas <roberto.huertas@outlook.com>
2018-08-25 00:54:26 +0200

    more functional

--- mdlint/src/rules/common_checks.rs
+++ mdlint/src/rules/common_checks.rs
@@ -5,33 +5,26 @@ use regex::Regex;
 
 crate fn check_content<'a>(root: &'a AstNode<'a>, regex: &str) -> Vec<RuleResultDetails> {
     let nodes = flatten_nodes_with_content(root);
-
-    let mut details: Vec<RuleResultDetails> = Vec::new();
     let rx = Regex::new(regex).unwrap();
-    nodes.iter().for_each(|x| {
-        let content = extract_content(x);
-        // println!("{:?}", x.data.borrow());
-        // println!("\nINNER \n");
-        // x.children().for_each(|c| println!("{:?}", c.data.borrow()));
+    nodes.iter().fold(Vec::new(), |mut acc, &n| {
+        let content = extract_content(n);
         let mut last_parsed_line = 0;
         content.split('\n').filter(|l| !l.is_empty()).for_each(|l| {
             if rx.is_match(l) {
-                // println!("\n------- \n");
-                // println!("{}", content);
-                let node = x.data.borrow();
+                let node = n.data.borrow();
                 last_parsed_line = if last_parsed_line == node.start_line {
                     last_parsed_line += 1;
                     last_parsed_line
                 } else {
                     node.start_line
                 };
-                details.push(RuleResultDetails::new(
+                acc.push(RuleResultDetails::new(
                     last_parsed_line,
                     node.start_column,
                     l.to_string(),
                 ));
             }
         });
-    });
-    details
+        acc
+    })
 }
